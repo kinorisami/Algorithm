6096a41df9bef8ebdfe82ca06dc6b8296f222c4e
kinorisa <kinorisami@gmail.com>
2024-12-09 21:25:25 +0800

    快速排序

---
+++ chap1/quick_sort.py
@@ -0,0 +1,30 @@
+def quick_sort(q, l, r):
+    if l >= r:
+        return
+
+    i = l - 1
+    j = r + 1
+    x = q[(l + r) // 2]
+
+    while i < j:
+        i += 1
+        while q[i] < x:
+            i += 1
+
+        j -= 1
+        while q[j] > x:
+            j -= 1
+
+        if i < j:
+            q[i], q[j] = q[j], q[i]
+
+    quick_sort(q, l, j)
+    quick_sort(q, j + 1, r)
+
+
+n = int(input())
+q = list(map(int, input().split()))
+
+quick_sort(q, 0, n - 1)
+
+print(' '.join(list(map(str, q))))
